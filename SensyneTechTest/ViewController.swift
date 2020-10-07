6876a7e9cea35ace6758c71c6b62e6a0962ff820
Marko Vukušić <marko@codesplitters.com>
2020-10-07 14:03:16 +0200

    Initial Commit

---
+++ SensyneTechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SensyneTechTest
+//
+//  Created by Marko Vukusic on 07/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
