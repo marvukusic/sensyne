c901406d5fd7821cfe21218e37f41d021619e89f
Marko Vukušić <marko@codesplitters.com>
2020-10-07 15:06:52 +0200

    Implement custom scene startup

--- SensyneTechTest/Scenes/HospitalsViewController.swift
+++ SensyneTechTest/Scenes/HospitalsViewController.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 class HospitalsViewController: UIViewController {
+    
+    var viewModel: HospitalsViewModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Hospital list"
     }
 
 
---
+++ SensyneTechTest/Scenes/HospitalsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HospitalsViewModel.swift
+//  SensyneTechTest
+//
+//  Created by Marko Vukusic on 07/10/2020.
+//
+
+import Foundation
+
+class HospitalsViewModel {
+    
+}
